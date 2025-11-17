81fb00ef69a9b3d1f8068f1818d64639db08a73a
drekain <drekainsmith@gmail.com>
2025-11-17 21:55:27 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,6 @@
-# Don't Remove Credit Tg - @Tushar0125
-# Ask Doubt on telegram @Tushar0125
-
 from os import environ
 
-API_ID = int(environ.get("API_ID", "")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
+API_ID = int(environ.get("API_ID", ""))  # Replace with your api id
+API_HASH = environ.get("API_HASH", ""))  # Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", ""))  # Replace with your bot token
+OWNER_ID = int(environ.get("OWNER_ID", "0"))  # Replace with your owner id
